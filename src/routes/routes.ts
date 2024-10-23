c6d2e42c48981c61ccd9f2a6b37f84a5c41bd7b1
saif <swaiflnkac@gmail.com>
2024-10-23 17:45:03 +0300

    feat(home): add navbar to the home page
    - add simple content to the home page
    
    Fix: #11

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home', { title: 'Home Page' });
+  res.render('home', { title: 'Home Page', path: '/' });
 });
 
 export default router;
