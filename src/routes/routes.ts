fad2cb553d9805531cda739e2a14b2edf936d9cb
saif <swaiflnkac@gmail.com>
2024-10-23 19:38:20 +0300

    feat(views): add login and signup views
    Fix: #7

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
   res.render('home', { title: 'Home Page', path: '/' });
 });
 
+router.get('/employee/signup', (req, res) => {
+  res.render('signup', { title: 'signup', path: '/signup' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'login', path: '/login' });
+});
+
 export default router;
