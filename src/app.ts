de22f7e1a26e9f6fee06dea10716b7d4c10ab095
saif <swaiflnkac@gmail.com>
2024-10-24 11:03:55 +0300

    chore: add body-parser to handle request body

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express from 'express';
 
-import homeRouter from './routes/routes';
+import viewsRouter from './routes/routes';
+
+import employeeRouter from './routes/employee.routes';
+
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.set('views', 'src/views');
 
-app.use(homeRouter);
+app.use(viewsRouter);
+
+app.use(employeeRouter);
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
