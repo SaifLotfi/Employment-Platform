782409eeb58e1622f83f1ef46b03a255188d4929
saif <swaiflnkac@gmail.com>
2024-10-25 11:31:38 +0300

    feat(views): add error alert message

--- src/utils/app-error.ts
+++ src/utils/app-error.ts
@@ -1,10 +1,18 @@
 export class AppError extends Error {
   public statusCode: number;
+  public page?: string;
+  public data?: { [key: string]: string|boolean };
 
-  constructor(message: string, statusCode: number) {
+  constructor(
+    message: string,
+    statusCode: number,
+    page?: string,
+    data?: { [key: string]: string|boolean }
+  ) {
     super(message);
     this.statusCode = statusCode;
-
-    //Error.captureStackTrace(this, this.constructor);
+    this.page = page;
+    this.data = data;
   }
 }
+
