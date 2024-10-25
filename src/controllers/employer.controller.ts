749fcee4e2b7448f8b09f60a63bc6a73ce0121cc
saif <swaiflnkac@gmail.com>
2024-10-25 17:09:17 +0300

    feat(auth): add employee login
    - check if nationalId is already registered in the database

--- src/controllers/employer.controller.ts
+++ src/controllers/employer.controller.ts
@@ -2,13 +2,14 @@ import { employerRepository } from "../models/employer.model";
 import { Request, Response } from "express";
 import { AppError } from "../utils/app-error";
 import { signJwt } from "../utils/jwt";
+import { isPasswordMatch } from "../utils/hash-password";
 
-const createEmployer = async (req:Request, res:Response) => {
+const registerEmployer = async (req:Request, res:Response) => {
   const existingEmployer = await employerRepository.getEmployer(req.body.email);
 
   if (existingEmployer) throw new AppError('Email is already taken', 400, {
     title: 'signup',
-    path: '/employer/signup',
+    path: '/signup',
     page: 'employer-signup',
   });
 
@@ -26,6 +27,36 @@ const createEmployer = async (req:Request, res:Response) => {
   res.redirect('/');
 };
 
+const loginEmployer = async (req:Request, res:Response) => {
+  const employer = await employerRepository.getEmployer(req.body.email);
+
+  if (!employer) throw new AppError('Invalid credentials', 400, {
+    title: 'login',
+    path: '/login',
+    page: 'employer-login',
+  });
+
+  const isMatch = await isPasswordMatch(req.body.password, employer.password);
+
+  if (!isMatch) throw new AppError('Invalid credentials', 400, {
+    title: 'login',
+    path: '/login',
+    page: 'employer-login',
+  });
+
+  const token = signJwt({ empId: employer.empId },'1h');
+
+  // Store the JWT in an HTTP-only cookie
+  res.cookie('token', token, {
+    httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+    secure: true, // Ensures the cookie is only sent over HTTPS
+    maxAge: 3600000 // 1 hour expiration
+  });
+
+  res.redirect('/');
+}
+
 export const employerController = {
-  createEmployer
+  registerEmployer,
+  loginEmployer
 }
--- src/routes/employer.routes.ts
+++ src/routes/employer.routes.ts
@@ -4,6 +4,8 @@ import { employerController } from '../controllers/employer.controller';
 
 const router = express.Router();
 
-router.post('/employer/signup',employerController.createEmployer);
+router.post('/employer/signup',employerController.registerEmployer);
+
+router.post('/employer/login',employerController.loginEmployer);
 
 export default router;
