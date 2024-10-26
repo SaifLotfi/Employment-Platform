73ea9ee0c2cf38dd9be3239f98924e5a3053a45a
saif <swaiflnkac@gmail.com>
2024-10-26 13:58:10 +0300

    feat: add title to employee model

---
+++ prisma/migrations/20241026094138_add_title_to_employee_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `title` to the `Employee` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Employee" ADD COLUMN     "title" TEXT NOT NULL;
