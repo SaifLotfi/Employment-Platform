4aeec39b4cd35f23b3d3980dd6128c27925e1290
saif <swaiflnkac@gmail.com>
2024-10-27 23:21:42 +0300

    feat(validation): add validation for post job route

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -2,10 +2,17 @@ import express from 'express';
 
 import { jobController } from '../controllers/job.controller';
 import { isAuth, isEmployee, isEmployer } from '../middlewares/auth.middleware';
+import { jobSchema, validate } from '../validation';
 
 const router = express.Router();
 
-router.post('/job', isAuth, isEmployer, jobController.postJob);
+router.post(
+  '/job',
+  isAuth,
+  isEmployer,
+  validate(jobSchema, 'Post Job', '/job/post', 'post-jobs'),
+  jobController.postJob
+);
 
 router.post('/job/:jobId/apply', isAuth, isEmployee, jobController.applyForAJob);
 
